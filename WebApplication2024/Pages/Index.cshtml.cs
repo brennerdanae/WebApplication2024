eb393d827eca145c6e9a119dbf710a9246607a50
Danae <danae.brenner@gmail.com>
2024-07-06 19:31:47 -0400

    updates to Index and Guest class added API key

--- WebApplication2024/Pages/Index.cshtml.cs
+++ WebApplication2024/Pages/Index.cshtml.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
 
 namespace WebApplication2024.Pages
 {
     public class IndexModel : PageModel
     {
+
+        static readonly HttpClient client = new HttpClient();
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -14,7 +19,50 @@ namespace WebApplication2024.Pages
 
         public void OnGet()
         {
+            List<Guest> guests = GetGuestData();
+            ViewData["Guests"] = guests;
 
+            var config = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+            string apikey = config["apikey"];
+            ViewData["apikey"] = apikey;
+        }
+
+        private List<Guest> GetGuestData()
+        {
+            string brand = "Web App";
+            string inBrand = Request.Query["Brand"];
+            if (inBrand != null && inBrand.Length > 0)
+            {
+                brand = inBrand;
+            }
+            int year = 2024;
+            ViewData["Brand"] = brand + year;
+            var task = client.GetAsync("https://jsonplaceholder.typicode.com/users");
+            HttpResponseMessage result = task.Result;
+            List<Guest> guests = new List<Guest>();
+            if (result.IsSuccessStatusCode)
+            {
+                Task<string> readString = result.Content.ReadAsStringAsync();
+                string jsonString = readString.Result;
+                JSchema schema = JSchema.Parse(System.IO.File.ReadAllText("guestschema.json"));
+                JArray jsonArray = JArray.Parse(jsonString);
+                IList<string> validationEvents = new List<string>();
+                if (jsonArray.IsValid(schema, out validationEvents))
+                {
+                    guests = Guest.FromJson(jsonString);
+                }
+                else
+                {
+                    foreach (string evt in validationEvents)
+                    {
+                        Console.WriteLine(evt);
+                    }
+                }
+            }
+
+            return guests;
         }
     }
 }
