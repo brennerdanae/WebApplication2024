693848db9f3db9c1a9472025525a183ca49b6bc1
Danae <danae.brenner@gmail.com>
2024-07-07 18:10:38 -0400

    working on JWT authentication

--- WebApplication2024/Controllers/ValuesController.cs
+++ WebApplication2024/Controllers/ValuesController.cs
@@ -3,6 +3,14 @@ using Microsoft.AspNetCore.DataProtection.KeyManagement.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using NHibernate.Cache;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,25 +26,25 @@ namespace WebApplication2024.Controllers
             _cacheProvider = cacheProvider;
         }
 
-        [Route("getAllGuest")]
-        public IActionResult GetAllGuest()
-        {
-            try
-            {
-                var guests = _cacheProvider.GetCachedResponse().Result;
-                return Ok(guests);
-            }
-            catch (Exception ex)
-            {
-                return new ContentResult()
-                {
-                    StatusCode = 500,
-                    Content = "{ \n error : " + ex.Message + "}",
-                    ContentType = "application/json"
-                };
-            }
+        //[Route("getAllGuest")]
+        //public IActionResult GetAllGuest()
+        //{
+        //    try
+        //    {
+        //        var guests = _cacheProvider.GetCachedResponse().Result;
+        //        return Ok(guests);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return new ContentResult()
+        //        {
+        //            StatusCode = 500,
+        //            Content = "{ \n error : " + ex.Message + "}",
+        //            ContentType = "application/json"
+        //        };
+        //    }
 
-        }
+        //}
 
         // GET: api/<ValuesController>
         [HttpGet]
@@ -69,5 +77,57 @@ namespace WebApplication2024.Controllers
         public void Delete(int id)
         {
         }
+    }
+
+    public class LoginController: Controller
+    {
+        private IConfiguration _config;
+        
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserModel login)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(login);
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+            return response;
+        }
+
+        private string GenerateJSONWebToken(UserModel userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Issuer"],
+                null,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+
+            //validate the User Credentials demo
+            if(login.Username == "Danae")
+            {
+                user = new UserModel { Username = "Danae Brenner", EmailAddress = "danae.brenner@gmail.com" };
+            }
+            return user;
+
+        }
+
     }
 }
--- WebApplication2024/Program.cs
+++ WebApplication2024/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,6 +21,21 @@ builder.Services.AddSwaggerGen( options =>
     
     );
 builder.Services.AddMemoryCache();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = Configuration["Jwt:Issuer"],
+            ValidAudience = Configuration["Jwt.Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+        };
+    });
+builder.Services.AddMvc();
 
 var app = builder.Build();
 
@@ -39,6 +59,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseMvc();
 
 app.MapRazorPages();
 
