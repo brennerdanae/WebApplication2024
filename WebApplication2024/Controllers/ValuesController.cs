2408450ab94db95c3cb16fd9a2aeee08ad175b27
Danae <danae.brenner@gmail.com>
2024-07-08 07:08:22 -0400

    Update CRUD values with db scenario

--- WebApplication2024/Controllers/ValuesController.cs
+++ WebApplication2024/Controllers/ValuesController.cs
@@ -20,6 +20,14 @@ namespace WebApplication2024.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        /*
+         *private readonly DbContext _context
+         *public ValuesController(DbContext context)
+         *{
+         *  _context = context;
+         *}
+         */
+
         private ICacheProvider _cacheProvider;
         public ValuesController(ICacheProvider cacheProvider)
         {
@@ -53,6 +61,27 @@ namespace WebApplication2024.Controllers
             return GuestRoster.allGuests;
         }
 
+        /*
+         *[HttpGet]
+         *[Authorize]
+         *public IActionResult Get()
+         *{
+         *  try
+         *  {
+         *      var guests = _context.Guests.ToList();
+         *      if(guests.Count == 0)
+         *      {
+         *          return NotFound("No data Found.");
+         *      }
+         *      return Ok(guests);
+         *  }
+         *  catch (Exception ex)
+         *  {
+         *  return BadRequest(ex.Message);
+         *  }
+         */
+
+
         // GET api/<ValuesController>/5
         [HttpGet("{id}")]
         public Guest Get(int id)
@@ -60,11 +89,50 @@ namespace WebApplication2024.Controllers
             return GuestRoster.allGuests[id];
         }
 
+        /*
+         * [HttpGet("{id}")]
+         * public IActionResult Get(int id)
+         * {
+         *      try
+         *      {
+         *          var guest = _context.Guests.Find(id);
+         *          if (guest == null)
+         *          {
+         *              return NotFound($"Guest not found with id {id}");
+         *          }
+         *          return Ok(guest);
+         *      }
+         *      catch  (Exception ex)
+         *      {
+         *          BadRequest(ex.Message);
+         *      }
+         * }
+         */
+
         // POST api/<ValuesController>
         [HttpPost]
         public void Post([FromBody] Guest value)
         {
         }
+
+        /*
+         *[HttpPost]
+         * public IActionResult Post(Guest value)
+         * {
+         *      try
+         *      {
+         *      
+         *          _context.Add(value);
+         *          _context.SaveChanges();
+         *          
+         *          return Ok("Guest Created);
+         *      }
+         *      catch (Exception ex)
+         *      {
+         *          return BadRequest(ex.Message);
+         *      }
+         * }
+         */
 
         // PUT api/<ValuesController>/5
         [HttpPut("{id}")]
@@ -72,11 +140,71 @@ namespace WebApplication2024.Controllers
         {
         }
 
+        /*
+         *[HttpPut("{id}")]
+         *public IActionResuolt Post(Guest value)
+         *{
+         *  if (value==null || value.id==0)
+         *  {
+         *      if (value==null)
+         *      {
+         *          return BadRequest("Guest data is invalid");
+         *      }
+         *      if (value.id ==0)
+         *      {
+         *          return BadRequest($"Guest id {value.id} is invalid");
+         *      }
+         *  }
+         *  try
+         *  {
+         *      var value = _context.Guests.Find(value.id);
+         *      if(value ==null)
+         *      {
+         *          return BadRequest("");
+         *      }
+         *      value.
+         *      value.
+         *      value.
+         *      value.
+         *      value.
+         *      _context.SaveChanges();
+         *      
+         *      return Ok("Guest details updated.");
+         *  }
+         *  catch (Exception ex)
+         *  {
+         *      return BadRequest(ex.Message);
+         *  }
+         *}
+         */
+
         // DELETE api/<ValuesController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
         }
+
+        /*
+         *[HttpDelete("{id}")]
+         *public IActionResult Delete(int id)
+         *{
+         *  try
+         *  {
+         *      var guest = _context.Guests.Find(id);
+         *      if (guest == null)
+         *      {
+         *          return NotFound($"Guest not found with id {id}");
+         *      }
+         *      _context.Guests.remove(guest);
+         *      _context.SaveChanges();
+         *      return Ok("Guest deleted.");
+         *  }
+         *  catch (Exception ex)
+         *  {
+         *      return BadRequest(ex.Message);
+         *  }
+         *}
+         */
     }
 
 }
